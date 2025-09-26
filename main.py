2299a143418654b5d4df147262ac1b215bed4dbf
caian23 <caian23@outlook.com>
2025-09-26 12:31:09 -0300

    feat: script agora recebe 2 argumentos. O segundo argumento indica o tipo de conteudo a ser usado para geração do QRCode

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import qrcode
 import sys
+import requests
+import os
 
+def tratar_url(arg):
+    if arg.find("https://") == -1 or arg.find("http://") == -1:
+        arg = "https://"+arg
+    if arg.find(".com") == -1 or arg.find(".org") == -1:
+        arg = arg + ".com"
+    return arg
 
-def gerar_qr_code(link):
-    img = qrcode.make(link)
+def testar_url(link):
+    try:
+        response = requests.get(link)
+    except:
+        print("Link inválido...")
+        exit()
+
+def gerar_qr_code(*conteudo):
+    if len(conteudo) == 2:
+        img = qrcode.make(conteudo[1])
+    else:
+        img = qrcode.make(conteudo[0])
     type(img) 
-    img.save(link+".png")
+    nome_arquivo_gerado=conteudo[0]+".png"
+    img.save(nome_arquivo_gerado)
+    print(f"QRCode gerado e salvo em: {os.getcwd()}/{nome_arquivo_gerado}")
 
 def main():
-    if len(sys.argv) > 1:
-        link = sys.argv[1]
-        gerar_qr_code(link)
+    if len(sys.argv) > 2:
+        conteudo = sys.argv[1]
+        if sys.argv[2] == "link":
+            url = tratar_url(conteudo)
+            testar_url(url)
+        gerar_qr_code(conteudo, url)
     else:
-        print(f"Uso {sys.argv[0]} <exemplo.com>")
+        print(f"Uso {sys.argv[0]} <conteudo> <tipo (escolher entre link, texto)>")
 
 if __name__ == "__main__":
     main()
